7cb1c3b4da59056fffeb3c9965562480e385126c
jalnyr <o.jalmari.vesterinen@gmail.com>
2026-05-03 12:11:22 +0300

    squirrel armor

--- src/main/java/net/jalnyr/experiencedfood/entity/client/SquirrelModel.java
+++ src/main/java/net/jalnyr/experiencedfood/entity/client/SquirrelModel.java
@@ -56,7 +56,9 @@ public class SquirrelModel<T extends Entity> extends HierarchicalModel<T> {
         PartDefinition torso = body.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -5.0F, -4.0F, 4.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
         PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 10).addBox(-2.0F, -2.0F, -3.0F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 21).addBox(-1.0F, 0.0F, -4.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, -3.0F));
+                .texOffs(0, 21).addBox(-1.0F, 0.0F, -4.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(2.0F, -3.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-3.0F, -3.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, -3.0F));
 
         PartDefinition leg = body.addOrReplaceChild("leg", CubeListBuilder.create().texOffs(10, 16).addBox(0.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
                 .texOffs(8, 23).addBox(0.0F, 2.0F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, -3.0F, -3.0F));
--- src/main/resources/assets/mob/Squirrel.java
+++ src/main/resources/assets/mob/Squirrel.java
@@ -3,7 +3,7 @@
 // Paste this class into your mod and generate all required imports
 
 
-public class squirrel<T extends Entity> extends EntityModel<T> {
+public class Squirrel<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "squirrel"), "main");
 	private final ModelPart squirrel;
@@ -16,7 +16,7 @@ public class squirrel<T extends Entity> extends EntityModel<T> {
 	private final ModelPart leg2;
 	private final ModelPart tail;
 
-	public squirrel(ModelPart root) {
+	public Squirrel(ModelPart root) {
 		this.squirrel = root.getChild("squirrel");
 		this.body = this.squirrel.getChild("body");
 		this.leg3 = this.body.getChild("leg3");
@@ -45,7 +45,9 @@ public class squirrel<T extends Entity> extends EntityModel<T> {
 		PartDefinition torso = body.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -5.0F, -4.0F, 4.0F, 3.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 10).addBox(-2.0F, -2.0F, -3.0F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 21).addBox(-1.0F, 0.0F, -4.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, -3.0F));
+		.texOffs(0, 21).addBox(-1.0F, 0.0F, -4.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(2.0F, -3.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(-3.0F, -3.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, -3.0F));
 
 		PartDefinition leg = body.addOrReplaceChild("leg", CubeListBuilder.create().texOffs(10, 16).addBox(0.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
 		.texOffs(8, 23).addBox(0.0F, 2.0F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, -3.0F, -3.0F));
